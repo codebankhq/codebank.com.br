dd14c2e93adc9ab1475fe55eda5aacf937fe0572
GuilhermehChaves <contato.guilhermehchaves@gmail.com>
2020-07-17 16:53:28 -0300

    selecting options
    selecting between bank transfer and bank slip

---
+++ static/js/highlight.js
@@ -0,0 +1,8 @@
+$("pre code").each(function(){
+    var html = $(this).html();
+    var pattern = html.match(/\s*\n[\t\s]*/);
+    $(this).html(html.replace(new RegExp(pattern, "g"),'\n'));
+  });
+
+  hljs.initHighlightingOnLoad();
+  hljs.initLineNumbersOnLoad();
