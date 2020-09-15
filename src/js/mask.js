b05dda4d994daf8762d41ef25884991d5aa47fb8
GuilhermehChaves <contato.guilhermehchaves@gmail.com>
2020-09-15 17:04:17 -0300

    máscara de telefone
    com base no code review adicionando máscara no campo de telefone ao
    digitar

---
+++ src/js/mask.js
@@ -0,0 +1,13 @@
+jQuery(function ($) {
+    const cellphone = $("#cellphone");
+    cellphone.mask("(99) 9?9999-9999", { placeholder: "_" });
+    
+    cellphone.on('focus click', function () {
+        var regex = new RegExp('[0-9]+');
+        const value = $(this)[0].value;
+
+        if (!regex.test(value)) {
+            $(this)[0].setSelectionRange(0, 0);
+        }
+    })
+});
