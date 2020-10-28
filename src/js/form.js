df6614114e4fda0dc7c028d8f03676666d381b86
GuilhermehChaves <contato.guilhermehchaves@gmail.com>
2020-10-28 14:39:03 -0300

    criando classe para validação
    com base no code review, criando uma classe para fazer as validações do
    formulário

--- src/js/form.js
+++ src/js/form.js
@@ -1,80 +1,103 @@
 const phoneParse = require('telefone/parse');
 
-$("form").on("submit", function (event) {
-    event.preventDefault();
+class FormControl {
+    constructor(selector) {
+        this.form = $(selector);
 
-    function isEmpty(string) {
+        this.name = $(this.form[0][0]);
+        this.email = $(this.form[0][1]);
+        this.phone = $(this.form[0][2]);
+    }
+
+    submit() {
+        if (this.validate()) {
+            const body = {
+                "name": this.name.val(),
+                "email": this.email.val(),
+                "phone": this.phone.val(),
+            }
+
+            $.ajax({
+                type: "POST",
+                url: process.env.LEAD_URL,
+                data: JSON.stringify(body),
+                success: this.onSuccess.bind(this),
+                error: this.onError.bind(this)
+            });
+        };
+    }
+
+    validate() {
+        let valid = true;
+        const borders = $(".border--input");
+
+        if (this.isEmpty(this.name.val())) {
+            this.inputError(borders[0], this.name);
+            this.errorMessage("Informe o seu nome.");
+            this.name.error = true;
+            valid = false;
+        }
+
+        if (this.isEmpty(this.email.val())) {
+            this.inputError(borders[1], this.email);
+            this.errorMessage("Informe o seu email.");
+            this.email.error = true;
+            valid = false;
+        }
+
+        if (this.phone.val() && phoneParse(this.phone.val()) == null) {
+            this.inputError(borders[2], this.phone);
+            this.errorMessage("Informe um número válido.");
+            this.phone.error = true;
+            valid = false;
+        }
+
+        return valid;
+    }
+
+    isEmpty(string) {
         const regex = new RegExp(/^\s+$/);
         return string == "" || regex.test(string);
     }
 
-    function inputError(border, input) {
+    inputError(border, input) {
         $(border).css("border", "1px solid #E04646");
-        $(input).css("border", "1px solid #E04646");
+        input.css("border", "1px solid #E04646");
     }
 
-    function errorMessage(msg) {
+    errorMessage(msg) {
         let message = $(".register__message");
         message.css("display", "block");
         message.html(msg);
     }
 
-    const body = {
-        "name": event.target[0].value,
-        "email": event.target[1].value,
-        "phone": event.target[2].value,
+    onError(_) {
+        this.errorMessage("Ocorreu um erro inesperado, tente novamente mais tarde.");
     }
 
-    const inputs = $(".border--input");
-
-    if (isEmpty(body.name)) {
-        inputError(inputs[0], inputs[0].firstElementChild);
-        errorMessage("Informe o seu nome.");
-        return;
-    }
-
-    if (isEmpty(body.email)) {
-        inputError(inputs[1], inputs[1].firstElementChild);
-        errorMessage("Informe o seu email.");
-        return;
-    }
-
-    if (body.phone && phoneParse(body.phone) == null) {
-        inputError(inputs[2], inputs[2].firstElementChild);
-        errorMessage("Informe um número válido.");
-        return;
-    }
-
-    function onSuccess(response) {
+    onSuccess(response) {
         if (response.status == "success") {
             const title = $("#register .title");
             const subtitle = $(".register__request-account");
 
             $(title[1]).html("<span class='marked'> Cadastrado com sucesso! </span>");
-            $("form").css("display", "none");
+            this.form.css("display", "none");
 
-            if(subtitle.css("display") == "none") {
+            if (subtitle.css("display") == "none") {
                 subtitle.css("display", "block");
             }
 
             subtitle.html("Ficamos felizes com o seu interesse! Toda nova atualização, lembraremos de enviar à você!");
 
         } else {
-            errorMessage("Ocorreu um erro inesperado, tente novamente mais tarde.");
+            this.errorMessage("Ocorreu um erro inesperado, tente novamente mais tarde.");
         }
     }
+}
 
-    function onError(_) {
-        errorMessage("Ocorreu um erro inesperado, tente novamente mais tarde.");
-    }
+const formControl = new FormControl("form");
 
-    $.ajax({
-        type: "POST",
-        url: process.env.LEAD_URL,
-        data: JSON.stringify(body),
-        success: onSuccess,
-        error: onError
-    });
-
-    return;
-})
+$("form").on("submit", function (event) {
+    event.preventDefault();
+    formControl.submit();
+});
